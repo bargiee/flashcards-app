3d0034c7657e0ed337fafef05192c022eb2d9e81
Bartosz Grzybowski <bjgrzybowski@gmail.com>
2025-03-19 16:32:47 +0100

    feat(router): add flashcard routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import userRoutes from './userRoutes';
 import deckRoutes from './deckRoutes';
+import flashcardRoutes from './flashcardRoutes';
 
 const router = Router();
 
 router.use('/users', userRoutes);
 router.use('/decks', deckRoutes);
+router.use('/flashcards', flashcardRoutes);
 
 export default router;
