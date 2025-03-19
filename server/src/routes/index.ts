cc6d8492073040cbfdb718c8e246388f4e2c8588
Bartosz Grzybowski <bjgrzybowski@gmail.com>
2025-03-19 16:11:56 +0100

    feat: add deck routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import userRoutes from './userRoutes';
+import deckRoutes from './deckRoutes';
 
 const router = Router();
 
 router.use('/users', userRoutes);
+router.use('/decks', deckRoutes);
 
 export default router;
