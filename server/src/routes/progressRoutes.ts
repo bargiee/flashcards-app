87a1ee60bc8758f440bd45c849e08830f2a53f1f
Bartosz Grzybowski <bjgrzybowski@gmail.com>
2025-04-02 15:39:01 +0200

    add progress routes

---
+++ server/src/routes/progressRoutes.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import {
+    getProgressByUser,
+    createOrUpdateProgress,
+    deleteProgress,
+} from '../controllers/progressController';
+
+const router = Router();
+
+router.get('/user/:userId', getProgressByUser);
+router.post('/', createOrUpdateProgress);
+router.delete('/:id', deleteProgress);
+
+export default router;
