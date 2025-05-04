f1a03ee22575dce3dc59e8755828788932bab418
Bartosz Grzybowski <bjgrzybowski@gmail.com>
2025-05-04 20:19:01 +0200

    refactor: add routes for Create, Home and Library page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,9 @@ import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
 import StartPage from './pages/StartPage';
 import NotFoundPage from './pages/NotFoundPage';
+import CreateSetPage from './pages/CreateSetPage';
+import HomePage from './pages/HomePage';
+import LibraryPage from './pages/LibraryPage';
 
 function App() {
     return (
@@ -11,6 +14,9 @@ function App() {
             <Route path="/start" element={<StartPage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/signup" element={<SignUpPage />} />
+            <Route path="/create" element={<CreateSetPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/library" element={<LibraryPage />} />
             <Route path="*" element={<NotFoundPage />} />
         </Routes>
     );
